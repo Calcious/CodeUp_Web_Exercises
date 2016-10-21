08290e15c592e39381953a8991435157cbcf83aa
Justin Armer <justin.armer@gmail.com>
2016-10-21 14:00:18 -0500

    Jquery introduction selectors/multiple selectors

--- js/jquery_practice.js
+++ js/jquery_practice.js
@@ -1,10 +1,5 @@
 /**
  * Created by Calcious on 10/21/16.
  */
-$(document).ready(function() {
-    alert( 'The DOM has finished loading!' );
-});
-
-alert($("#header").html());
-
-alert($(".lineItem").html());
+$("li").css('font-size', '20px');
+$("h1, p, li").css('border', '1px solid #F00');
